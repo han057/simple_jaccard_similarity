321d61c807d50930d9dc50031c0d40b4c4058908
fredyhernan057@gmail.com <fredyhernan057@gmail.com>
2018-11-14 13:32:24 +0100

    Jaccard similarity between the documents using the signature matrix

--- similarity.py
+++ similarity.py
@@ -4,6 +4,7 @@ import logging
 import random
 from nltk.corpus import stopwords
 import numpy as np
+import time
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -29,16 +30,15 @@ def loadStopWords():
 
 def loadShingles(file, stop_word_tuple):
     index = 0
-    prime = 38
-    numberOfHashFunctions = 5
+    prime = 163
+    numberOfHashFunctions = 10
     universal_shingles = list()
     hashFunctions = hashFunctionsSet(prime, numberOfHashFunctions)
     articles = file.read().split("\n")
     numberOfArticles = len(articles)
-    numberOfArticles = 2
+    #numberOfArticles = numberOfArticles // 10
     signatureMatrix = np.full((numberOfArticles, numberOfHashFunctions), prime)
-    [[prime]*numberOfHashFunctions]*numberOfArticles
-    print(signatureMatrix)
+
     for a in range(numberOfArticles):
         article_shingles = set()
         words = articles[a].split(' ')
@@ -46,10 +46,8 @@ def loadShingles(file, stop_word_tuple):
         for w in range(1, numberOfWords):
             if words[w].lower() in stop_word_tuple:
                 shingle = words[w]
-                if((w+1) < numberOfWords):
-                    shingle += ' ' + words[w+1]
-                    if((w+2) < numberOfWords):
-                        shingle += ' ' + words[w+2]
+                if((w+2) <= numberOfWords):
+                    shingle = words[w+1] + ' ' + words[w+2]
                 try:
                     indexOfShingle = universal_shingles.index(shingle)
                 except:
@@ -63,11 +61,26 @@ def loadShingles(file, stop_word_tuple):
                         bucket = hashFunctions[h](indexOfShingle)
                         if bucket < signatureMatrix[a][h]:
                             signatureMatrix[a][h] = bucket
-    return (len(universal_shingles), numberOfArticles, signatureMatrix)
+    return (len(universal_shingles), numberOfArticles, signatureMatrix.T)
+
+
+def calculateJaccardSimilarity(sm):
+    print("Calculando jaccard similarity...")
+
+    permutations = sm.shape[0]
+    documents = sm.shape[1]
+    jaccardResultMatrix = np.zeros((documents, documents))
+    for i in range(documents):
+        di = sm[:, i]
+        for j in range(i + 1, documents):
+            dj = sm[:, j]
+            similarity = (di == dj).sum() / permutations
+            jaccardResultMatrix[i][j] = similarity
+    print(jaccardResultMatrix)
 
 
 def main():
-    with open('dataset_1.txt') as f:
+    with open('dataset_10.txt') as f:
         us, spf, sm = loadShingles(f, loadStopWords())
 
     print("""
@@ -77,5 +90,8 @@ def main():
     ******************************************
     """.format(spf, us))
     print(sm)
+    calculateJaccardSimilarity(sm)
 
+start = time.clock()
 main()
+print((time.clock() - start)/1000/60)
